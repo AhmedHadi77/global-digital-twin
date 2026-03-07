fcc1be4a8fe12624b32ce9607c9ac4fbae10ee0f
Ahmed Hadi <ahmed777hady@gmail.com>
2026-03-08 01:54:07 +0800

    initial digital twin project

---
+++ simulator/deviceSimulator.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+
+    const device = {
+        deviceId: "device-" + Math.floor(Math.random()*1000),
+        temperature: 20 + Math.random()*10,
+        humidity: 40 + Math.random()*20
+    }
+
+    console.log(device)
+
+}, 1000);
